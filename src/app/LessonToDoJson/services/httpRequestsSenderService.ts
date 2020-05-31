7bf4982b1005fd070f63ea5a69b6a891b8ea14c3
okuznetcova <olga_amp@mail.ru>
2020-05-31 19:18:16 +0300

    Introduce new service to download users or upload one user

---
+++ src/app/LessonToDoJson/services/httpRequestsSenderService.ts
@@ -0,0 +1,28 @@
+import {UserDto} from '../../UserDto';
+
+export class HttpRequestsSenderService {
+
+  private url: string = 'https://gorest.co.in/public-api/users?_format=json&access-token=Mpa9uWdhPEW_AbKAgwY8PHJHODpV84Cgo1d-';
+
+  getUsers(page: number, functionToExecuteWhenAllUsersDownloaded: (xhr: XMLHttpRequest) => void) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', this.url + '&page=' + page);
+    xhr.onload = function() {
+      functionToExecuteWhenAllUsersDownloaded(xhr);
+    };
+    xhr.send();
+  }
+
+  postUser(userDto: UserDto, functionToExecuteWhenUserUploadedToServer: () => void) {
+    let user = JSON.stringify(userDto);
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', this.url);
+    xhr.onload = function() {
+      functionToExecuteWhenUserUploadedToServer();
+    };
+    xhr.setRequestHeader('Content-type', 'application/json');
+
+    xhr.send(user);
+  }
+}
